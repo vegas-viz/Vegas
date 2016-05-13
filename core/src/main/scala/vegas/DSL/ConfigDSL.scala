34711565518fab1c241821a1fb15cdd615574d48
Aish Fenton <afenton@netflix.com>
2016-05-13 10:06:28 -0700

    full config + fixed spark jar bug

--- core/src/main/scala/vegas/DSL/ConfigDSL.scala
+++ core/src/main/scala/vegas/DSL/ConfigDSL.scala
@@ -12,10 +12,16 @@ trait ConfigDSL extends FieldExtractor {
   private val _config = GenLens[Spec](_.config)
   private val _cell = GenLens[Config](_.cell)
 
-  def configCell(width: OptArg[Int] = NoArg, height: OptArg[Int] = NoArg): SpecBuilder = {
-    val cell = Cell(width, height)
+  def configCell(width: OptArg[Int] = NoArg, height: OptArg[Int] = NoArg, fill: OptArg[String] = NoArg,
+                 fillOpacity: OptArg[Double] = NoArg, stroke: OptArg[String] = NoArg,
+                 strokeOpacity: OptArg[Double] = NoArg, strokeWidth: OptArg[Int] = NoArg,
+                 strokeDash: OptArg[Seq[Int]] = NoArg, strokeDashOffset: OptArg[Int] = NoArg) = {
+
+    val cell = Cell(width, height, fill, fillOpacity, stroke, strokeOpacity, strokeWidth, strokeDash, strokeDashOffset)
+
     (_spec composeLens _config composePrism _orElse(Config()) composeLens _cell).set(Some(cell))(this)
   }
 
-
 }
+
+
