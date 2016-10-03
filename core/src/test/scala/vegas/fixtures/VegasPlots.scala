1d6c217b09d3506be7b0f28ac224391c5db9ced7
Roger Menezes <rmenezes@netflix.com>
2016-10-03 14:32:53 -0700

    Changing disableBin to enableBin, adding more binned examples

--- core/src/test/scala/vegas/fixtures/VegasPlots.scala
+++ core/src/test/scala/vegas/fixtures/VegasPlots.scala
@@ -29,15 +29,15 @@ object VegasPlots {
           encodeY2("people", aggregate=Q3)
       )
 
-  val BinPlot =
-    Vegas("Plot with binning").
+  val BinnedPlot =
+    Vegas("Plot to show Binning options").
       withDataURL(Movies).
-      mark(Point).
-      encodeY("IMDB_Rating", Quantitative, bin=Bin(maxbins=10.0)).
-      encodeX("Rotten_Tomatoes_Rating", Quantitative, bin=Bin(maxbins=10.0)).
-      encodeSize(aggregate=Count, field="*", dataType=Quantitative)
+      mark(Bar).
+      encodeX("IMDB_Rating", Quantitative, bin=Bin(step=2.0, maxbins=3.0)).
+      encodeY(field="*", Quantitative, aggregate=Count)
 
-  val plots: List[SpecBuilder] = ValuePlot :: IQRPlot :: BinPlot :: Nil
+
+  val plots: List[SpecBuilder] = ValuePlot :: IQRPlot :: BinnedPlot :: Nil
 
 }
 
